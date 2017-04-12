a46ee8b3af2bc6c2fba576d40870555f3100302e
Carlos Luis <clburguete@gmail.com>
2017-04-12 16:02:16 +0200

    nearly done

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -29,6 +29,7 @@ var MemoryGame = function() {
   this.pairs_clicked = 0;
   this.pairs_guessed = 0;
   this._shuffleCard();
+  this.paintCards();
 };
 // this function just takes the array of cards above and shuffles them into a random order
 MemoryGame.prototype._shuffleCard = function() {
@@ -43,19 +44,42 @@ MemoryGame.prototype._shuffleCard = function() {
   }
   return;
 };
+//check if both elements in picked cards are equal
+MemoryGame.prototype.checkEqual = function(card1, card2){
+  if (card1 === card2) {
+    this.pairs_guessed++;
+    return true;
+  }else {
+    this.pairs_clicked++;
+    return false;
+  }
+};
 
-
-
-
-
-
-
+//dom manipulators
+MemoryGame.prototype.paintCards = function(){
+  this.Cards.forEach(function(currentValue){
+    var image = $("<div></div>").css("background-image", "url(img/"+currentValue.img+")");
+    var processedImage = image.attr({class: "pic", name:currentValue.name});
+    $("#memory_board").append(processedImage);
+  });
+};
+//create new game
 var memoryGame;
+
 $(document).ready(function(){
   memoryGame = new MemoryGame();
+  var card1, card2;
 
-
-
-
-
+  //player clicks, game starts evaluation
+  $(".pic").on("click", function (e){
+    if (!card1 && !card2) {
+      card1 = e.currentTarget.name;
+//      $(e.currentTarget).toggleClass("background-image", "none");
+    } else {
+      card2 = e.currentTarget.name;
+      console.log(memoryGame.checkEqual(card1,card2));
+      card1 = false;
+      card2 = false;
+    }
+  });
 });
